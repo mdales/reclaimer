b6b49f8c2d6d0b2567fa3a178465d017f8a8c9dc
Michael Dales <mwd24@cam.ac.uk>
2024-08-19 13:44:07 +0100

    Add fake test CLMS API key

--- clms/auth_test.go
+++ clms/auth_test.go
@@ -1,18 +1,10 @@
 package clms
 
 import (
-	"fmt"
-	"os"
 	"testing"
 )
 
 func TestLoadingCLMSAPIToken(t *testing.T) {
-	cwd, err := os.Getwd()
-	if nil != err {
-		t.Fatalf("Failed to get CWD: %v", err)
-	}
-	fmt.Printf("cwd: %s\n", cwd)
-
 	token, err := LoadAPIKey("testdata/exampleapi.key")
 	if nil != err {
 		t.Errorf("Failed to load key: %v", err)
